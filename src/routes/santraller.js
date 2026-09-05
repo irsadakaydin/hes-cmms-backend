d966956b68b775dde177d9510130d3f016f24df7
irsadakaydin <irsadakaydin@hotmail.com>
2026-09-05 22:07:02 +0300

    Add files via upload

--- src/routes/santraller.js
+++ src/routes/santraller.js
@@ -28,8 +28,9 @@ router.get("/", async (req, res, next) => {
 router.get("/:santral_id", async (req, res, next) => {
   try {
     const { rows } = await req.db.query(
-      `SELECT s.*
+      `SELECT s.*, i.ad AS isletme_adi
        FROM santral s
+       JOIN isletme i ON i.isletme_id = s.isletme_id
        WHERE s.santral_id = $1
          AND s.santral_id IN (
            SELECT santral_id FROM v_kullanici_yetkili_santraller WHERE kullanici_id = $2
